8b12c432c48d957ea3b7db9d8a4cc870ab143bb2
srt775 <srt775@inlumine.ual.es>
2025-11-01 14:44:49 +0100

    Delete Pr1_AnalisisDominioTiempo directory

--- Pr1_AnalisisDominioTiempo/Ejercicio 1.1/calcularRMS.m
+++
@@ -1,6 +0,0 @@
-function v_rms = calcularRMS(senal)
-		% Esta función debe guardarse en un archivo separado 'calcularRMS.m'
-		cuadrados = senal.^2;
-		media = mean(cuadrados);
-		v_rms = sqrt(media);
-		end
